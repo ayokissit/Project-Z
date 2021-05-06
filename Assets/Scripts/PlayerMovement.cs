da26e648ad27d0777ef509a1b7b889e8cecfc876
ayokissit <nikolaev.nikita0209@gmail.com>
2021-05-07 00:37:50 +0500

    first commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private float speed = 20f;
+    private Vector2 movement;
+    Animator playerAnimator;
+    private bool isFacingRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Walk();
+
+        if (isFacingRight == false && movement.x > 0)
+        {
+            RotatePlayer();
+        }
+        else if (isFacingRight == true && movement.x < 0)
+        {
+            RotatePlayer();
+        }
+
+        playerAnimator.SetBool("Run", Mathf.Abs(movement.x) >= 0.1f);
+
+    }
+
+    void RotatePlayer()
+    {
+        isFacingRight = !isFacingRight;
+
+        Vector3 playerScale = transform.localScale;
+        playerScale.x *= -1;
+
+        transform.localScale = playerScale;
+    }
+
+    void Walk()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+        
+    }
+}
---
+++ Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTrigger : MonoBehaviour
+{
+    public GameObject Text;
+    public GameObject Sphere;
+    public Text LabelName;
+
+    void Start()
+    {
+        Text.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+            Text.SetActive(true);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        var playerPos = Camera.main.WorldToScreenPoint(Sphere.transform.position);
+        LabelName.transform.position = playerPos;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Text.SetActive(false);
+    }
+}
