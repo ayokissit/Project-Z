be17e767e73badeac13148c38ad116967f6cf915
ayokissit <nikolaev.nikita0209@gmail.com>
2021-05-14 20:11:20 +0500

    website & inteface

--- Assets/Scripts/TextTrigger.cs
+++ Assets/Scripts/TextTrigger.cs
@@ -6,6 +6,7 @@ public class TextTrigger : MonoBehaviour
     public GameObject Text;
     public GameObject Sphere;
     public Text LabelName;
+    public bool IsPlayerInTrigger { get; set; }
 
     void Start()
     {
@@ -15,17 +16,27 @@ public class TextTrigger : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
+        {
+            GetPlayerPosition();
             Text.SetActive(true);
+            IsPlayerInTrigger = true;
+        }
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
-        var playerPos = Camera.main.WorldToScreenPoint(Sphere.transform.position);
-        LabelName.transform.position = playerPos;
+        GetPlayerPosition();
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         Text.SetActive(false);
+        IsPlayerInTrigger = false;
+    }
+
+    void GetPlayerPosition()
+    {
+        var playerPos = Camera.main.WorldToScreenPoint(Sphere.transform.position);
+        LabelName.transform.position = playerPos;
     }
 }
---
+++ Assets/Scripts/UseComputerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UseComputerScript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    void Update()
+    {
+        var trigger = GetComponent<TextTrigger>();
+
+        if (Input.GetKeyDown(KeyCode.E) && trigger.IsPlayerInTrigger) 
+        {
+            if (!GameIsPaused)
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Close()
+    {
+        if (GameIsPaused)
+        {
+            PauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+        }
+    }
+    
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadRegister()
+    {
+        Debug.Log("suck");
+    }
+}
